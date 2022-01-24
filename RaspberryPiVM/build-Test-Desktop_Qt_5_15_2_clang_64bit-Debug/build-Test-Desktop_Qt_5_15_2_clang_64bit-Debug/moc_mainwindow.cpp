de2160fe0b8814d99aadd143d1fbfc4531728c9a
Jin <30644474+Jin24@users.noreply.github.com>
2022-01-24 00:44:02 -0500

    Add files via upload

---
+++ RaspberryPiVM/build-Test-Desktop_Qt_5_15_2_clang_64bit-Debug/build-Test-Desktop_Qt_5_15_2_clang_64bit-Debug/moc_mainwindow.cpp
@@ -0,0 +1,329 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../gui/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[46];
+    char stringdata0[1283];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 23), // "on_login_button_clicked"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 17), // "on_cancel_clicked"
+QT_MOC_LITERAL(4, 54, 26), // "on_register_button_clicked"
+QT_MOC_LITERAL(5, 81, 17), // "on_signup_clicked"
+QT_MOC_LITERAL(6, 99, 22), // "on_exit_button_clicked"
+QT_MOC_LITERAL(7, 122, 17), // "on_logOut_clicked"
+QT_MOC_LITERAL(8, 140, 25), // "on_searchListings_clicked"
+QT_MOC_LITERAL(9, 166, 29), // "on_searchMenuPrevious_clicked"
+QT_MOC_LITERAL(10, 196, 22), // "on_seeAllTools_clicked"
+QT_MOC_LITERAL(11, 219, 21), // "on_myListings_clicked"
+QT_MOC_LITERAL(12, 241, 29), // "on_searchAllToolsRent_clicked"
+QT_MOC_LITERAL(13, 271, 36), // "on_searchByPricePreviousMenu_..."
+QT_MOC_LITERAL(14, 308, 37), // "on_searchAllToolsPreviousMenu..."
+QT_MOC_LITERAL(15, 346, 29), // "on_searchByPriceRange_clicked"
+QT_MOC_LITERAL(16, 376, 24), // "on_searchByPrice_clicked"
+QT_MOC_LITERAL(17, 401, 28), // "on_searchByPriceRent_clicked"
+QT_MOC_LITERAL(18, 430, 31), // "on_lowestToHighestPrice_clicked"
+QT_MOC_LITERAL(19, 462, 31), // "on_highestToLowestPrice_clicked"
+QT_MOC_LITERAL(20, 494, 29), // "on_returnToMainMenu_2_clicked"
+QT_MOC_LITERAL(21, 524, 27), // "on_returnToMainMenu_clicked"
+QT_MOC_LITERAL(22, 552, 26), // "on_borrowedListing_clicked"
+QT_MOC_LITERAL(23, 579, 21), // "on_changeInfo_clicked"
+QT_MOC_LITERAL(24, 601, 22), // "on_update_info_clicked"
+QT_MOC_LITERAL(25, 624, 27), // "on_cancelUserUpdate_clicked"
+QT_MOC_LITERAL(26, 652, 35), // "on_pushButton_createListing_c..."
+QT_MOC_LITERAL(27, 688, 21), // "on_newListing_clicked"
+QT_MOC_LITERAL(28, 710, 36), // "on_createListingPreviousMenu_..."
+QT_MOC_LITERAL(29, 747, 39), // "on_searchByLocationPreviousMe..."
+QT_MOC_LITERAL(30, 787, 31), // "on_searchByLocationRent_clicked"
+QT_MOC_LITERAL(31, 819, 35), // "on_searchByLocationProvince_c..."
+QT_MOC_LITERAL(32, 855, 27), // "on_searchByLocation_clicked"
+QT_MOC_LITERAL(33, 883, 31), // "on_searchByLocationCity_clicked"
+QT_MOC_LITERAL(34, 915, 33), // "on_searchByLocationStreet_cli..."
+QT_MOC_LITERAL(35, 949, 34), // "on_searchByLocationAddress_cl..."
+QT_MOC_LITERAL(36, 984, 23), // "on_searchByName_clicked"
+QT_MOC_LITERAL(37, 1008, 24), // "on_searchByBrand_clicked"
+QT_MOC_LITERAL(38, 1033, 35), // "on_searchByNamePreviousMenu_c..."
+QT_MOC_LITERAL(39, 1069, 36), // "on_searchByBrandPreviousMenu_..."
+QT_MOC_LITERAL(40, 1106, 29), // "on_searchByNameSearch_clicked"
+QT_MOC_LITERAL(41, 1136, 27), // "on_searchByNameRent_clicked"
+QT_MOC_LITERAL(42, 1164, 30), // "on_searchByBrandSearch_clicked"
+QT_MOC_LITERAL(43, 1195, 28), // "on_searchByBrandRent_clicked"
+QT_MOC_LITERAL(44, 1224, 30), // "on_removeListingButton_clicked"
+QT_MOC_LITERAL(45, 1255, 27) // "on_returnToolButton_clicked"
+
+    },
+    "MainWindow\0on_login_button_clicked\0\0"
+    "on_cancel_clicked\0on_register_button_clicked\0"
+    "on_signup_clicked\0on_exit_button_clicked\0"
+    "on_logOut_clicked\0on_searchListings_clicked\0"
+    "on_searchMenuPrevious_clicked\0"
+    "on_seeAllTools_clicked\0on_myListings_clicked\0"
+    "on_searchAllToolsRent_clicked\0"
+    "on_searchByPricePreviousMenu_clicked\0"
+    "on_searchAllToolsPreviousMenu_clicked\0"
+    "on_searchByPriceRange_clicked\0"
+    "on_searchByPrice_clicked\0"
+    "on_searchByPriceRent_clicked\0"
+    "on_lowestToHighestPrice_clicked\0"
+    "on_highestToLowestPrice_clicked\0"
+    "on_returnToMainMenu_2_clicked\0"
+    "on_returnToMainMenu_clicked\0"
+    "on_borrowedListing_clicked\0"
+    "on_changeInfo_clicked\0on_update_info_clicked\0"
+    "on_cancelUserUpdate_clicked\0"
+    "on_pushButton_createListing_clicked\0"
+    "on_newListing_clicked\0"
+    "on_createListingPreviousMenu_clicked\0"
+    "on_searchByLocationPreviousMenu_clicked\0"
+    "on_searchByLocationRent_clicked\0"
+    "on_searchByLocationProvince_clicked\0"
+    "on_searchByLocation_clicked\0"
+    "on_searchByLocationCity_clicked\0"
+    "on_searchByLocationStreet_clicked\0"
+    "on_searchByLocationAddress_clicked\0"
+    "on_searchByName_clicked\0"
+    "on_searchByBrand_clicked\0"
+    "on_searchByNamePreviousMenu_clicked\0"
+    "on_searchByBrandPreviousMenu_clicked\0"
+    "on_searchByNameSearch_clicked\0"
+    "on_searchByNameRent_clicked\0"
+    "on_searchByBrandSearch_clicked\0"
+    "on_searchByBrandRent_clicked\0"
+    "on_removeListingButton_clicked\0"
+    "on_returnToolButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      44,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  234,    2, 0x08 /* Private */,
+       3,    0,  235,    2, 0x08 /* Private */,
+       4,    0,  236,    2, 0x08 /* Private */,
+       5,    0,  237,    2, 0x08 /* Private */,
+       6,    0,  238,    2, 0x08 /* Private */,
+       7,    0,  239,    2, 0x08 /* Private */,
+       8,    0,  240,    2, 0x08 /* Private */,
+       9,    0,  241,    2, 0x08 /* Private */,
+      10,    0,  242,    2, 0x08 /* Private */,
+      11,    0,  243,    2, 0x08 /* Private */,
+      12,    0,  244,    2, 0x08 /* Private */,
+      13,    0,  245,    2, 0x08 /* Private */,
+      14,    0,  246,    2, 0x08 /* Private */,
+      15,    0,  247,    2, 0x08 /* Private */,
+      16,    0,  248,    2, 0x08 /* Private */,
+      17,    0,  249,    2, 0x08 /* Private */,
+      18,    0,  250,    2, 0x08 /* Private */,
+      19,    0,  251,    2, 0x08 /* Private */,
+      20,    0,  252,    2, 0x08 /* Private */,
+      21,    0,  253,    2, 0x08 /* Private */,
+      22,    0,  254,    2, 0x08 /* Private */,
+      23,    0,  255,    2, 0x08 /* Private */,
+      24,    0,  256,    2, 0x08 /* Private */,
+      25,    0,  257,    2, 0x08 /* Private */,
+      26,    0,  258,    2, 0x08 /* Private */,
+      27,    0,  259,    2, 0x08 /* Private */,
+      28,    0,  260,    2, 0x08 /* Private */,
+      29,    0,  261,    2, 0x08 /* Private */,
+      30,    0,  262,    2, 0x08 /* Private */,
+      31,    0,  263,    2, 0x08 /* Private */,
+      32,    0,  264,    2, 0x08 /* Private */,
+      33,    0,  265,    2, 0x08 /* Private */,
+      34,    0,  266,    2, 0x08 /* Private */,
+      35,    0,  267,    2, 0x08 /* Private */,
+      36,    0,  268,    2, 0x08 /* Private */,
+      37,    0,  269,    2, 0x08 /* Private */,
+      38,    0,  270,    2, 0x08 /* Private */,
+      39,    0,  271,    2, 0x08 /* Private */,
+      40,    0,  272,    2, 0x08 /* Private */,
+      41,    0,  273,    2, 0x08 /* Private */,
+      42,    0,  274,    2, 0x08 /* Private */,
+      43,    0,  275,    2, 0x08 /* Private */,
+      44,    0,  276,    2, 0x08 /* Private */,
+      45,    0,  277,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_login_button_clicked(); break;
+        case 1: _t->on_cancel_clicked(); break;
+        case 2: _t->on_register_button_clicked(); break;
+        case 3: _t->on_signup_clicked(); break;
+        case 4: _t->on_exit_button_clicked(); break;
+        case 5: _t->on_logOut_clicked(); break;
+        case 6: _t->on_searchListings_clicked(); break;
+        case 7: _t->on_searchMenuPrevious_clicked(); break;
+        case 8: _t->on_seeAllTools_clicked(); break;
+        case 9: _t->on_myListings_clicked(); break;
+        case 10: _t->on_searchAllToolsRent_clicked(); break;
+        case 11: _t->on_searchByPricePreviousMenu_clicked(); break;
+        case 12: _t->on_searchAllToolsPreviousMenu_clicked(); break;
+        case 13: _t->on_searchByPriceRange_clicked(); break;
+        case 14: _t->on_searchByPrice_clicked(); break;
+        case 15: _t->on_searchByPriceRent_clicked(); break;
+        case 16: _t->on_lowestToHighestPrice_clicked(); break;
+        case 17: _t->on_highestToLowestPrice_clicked(); break;
+        case 18: _t->on_returnToMainMenu_2_clicked(); break;
+        case 19: _t->on_returnToMainMenu_clicked(); break;
+        case 20: _t->on_borrowedListing_clicked(); break;
+        case 21: _t->on_changeInfo_clicked(); break;
+        case 22: _t->on_update_info_clicked(); break;
+        case 23: _t->on_cancelUserUpdate_clicked(); break;
+        case 24: _t->on_pushButton_createListing_clicked(); break;
+        case 25: _t->on_newListing_clicked(); break;
+        case 26: _t->on_createListingPreviousMenu_clicked(); break;
+        case 27: _t->on_searchByLocationPreviousMenu_clicked(); break;
+        case 28: _t->on_searchByLocationRent_clicked(); break;
+        case 29: _t->on_searchByLocationProvince_clicked(); break;
+        case 30: _t->on_searchByLocation_clicked(); break;
+        case 31: _t->on_searchByLocationCity_clicked(); break;
+        case 32: _t->on_searchByLocationStreet_clicked(); break;
+        case 33: _t->on_searchByLocationAddress_clicked(); break;
+        case 34: _t->on_searchByName_clicked(); break;
+        case 35: _t->on_searchByBrand_clicked(); break;
+        case 36: _t->on_searchByNamePreviousMenu_clicked(); break;
+        case 37: _t->on_searchByBrandPreviousMenu_clicked(); break;
+        case 38: _t->on_searchByNameSearch_clicked(); break;
+        case 39: _t->on_searchByNameRent_clicked(); break;
+        case 40: _t->on_searchByBrandSearch_clicked(); break;
+        case 41: _t->on_searchByBrandRent_clicked(); break;
+        case 42: _t->on_removeListingButton_clicked(); break;
+        case 43: _t->on_returnToolButton_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 44)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 44;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 44)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 44;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
